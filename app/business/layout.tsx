961aa17ec7f1fa48779b6683a95a860a93f47692
Darylle Jumalon <daryllejumalon16@gmail.com>
2026-03-07 09:59:52 +0800

    feat: add ai chatbox

--- app/business/layout.tsx
+++ app/business/layout.tsx
@@ -1,16 +1,31 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
-import { BusinessHeader, BusinessSidebar } from '@/app/business/components';
+import {
+  BusinessHeader,
+  BusinessSidebar,
+  AIChatSheet,
+} from '@/app/business/components';
+import { cn } from '@/lib/utils';
 
 export default function BusinessLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [isAIChatOpen, setIsAIChatOpen] = useState(false);
+
+  useEffect(() => {
+    if (isAIChatOpen) {
+      document.body.classList.add('ai-chat-sheet-open');
+    } else {
+      document.body.classList.remove('ai-chat-sheet-open');
+    }
+  }, [isAIChatOpen]);
+
   return (
-    <div className="bg-background font-geist flex h-screen">
+    <div className="bg-background font-geist flex h-screen overflow-hidden">
       <SidebarProvider
         defaultOpen
         style={
@@ -22,12 +37,20 @@ export default function BusinessLayout({
       >
         <BusinessSidebar />
         <SidebarInset className="flex flex-1 flex-col overflow-hidden">
-          <BusinessHeader />
+          <BusinessHeader
+            onAIChatClick={() => setIsAIChatOpen((prev) => !prev)}
+          />
           <div className="flex-1 overflow-auto">
-            <div className="p-10">{children}</div>
+            <div className={`p-10 transition-all duration-300 ease-in-out`}>
+              {children}
+            </div>
           </div>
         </SidebarInset>
       </SidebarProvider>
+
+      <div className={cn('w-0 transition-all', isAIChatOpen && 'w-lg p-2')}>
+        <AIChatSheet />
+      </div>
     </div>
   );
 }

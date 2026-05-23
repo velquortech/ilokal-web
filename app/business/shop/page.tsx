c41f97c334372632b99f2fcc3e27b42dc53d53d7
ianDrilon03 <ian@findfoundation.org.au>
2026-05-23 12:13:35 +0800

    deleting some excess page

--- app/business/shop/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation';
-import { getMyBusinesses } from '@/lib/api/business/business';
-import { businessShopPath } from '@/config/routeConfig';
-
-export default async function ShopIndexRedirect() {
-  const business = await getMyBusinesses();
-  if (!business) redirect('/business');
-  redirect(businessShopPath(business.id));
-}

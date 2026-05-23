26091a3b9e55c75e43e172ba78fdb87d743ae9d9
ianDrilon03 <ian@findfoundation.org.au>
2026-05-23 14:29:16 +0800

    fixing build app and linters

---
+++ services/index.ts
@@ -0,0 +1,4 @@
+// DEPRECATED: This file is maintained for backward compatibility only.
+// New code should import directly from '@/lib/services'.
+// Existing code importing from here will continue to work during migration.
+export * from '../lib/services';
